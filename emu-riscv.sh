f437a8edfce0bea745794390da98800d5f75146f
Neo <mario.sieg.64@gmail.com>
2025-07-15 17:22:43 +0200

    Update emu-riscv.sh

--- emu-riscv.sh
+++ emu-riscv.sh
@@ -1,8 +1,8 @@
 # Install and run RISC-V emulator with Ubuntu 20.04.5 preinstalled image
 sudo apt install qemu-system-misc opensbi u-boot-qemu qemu-utils
 mkdir qemu-riscv && cd qemu-riscv
-wget https://cdimage.ubuntu.com/releases/20.04.4/release/ubuntu-20.04.5-preinstalled-server-riscv64+unmatched.img.xz
-xz -dk ubuntu-20.04.5-preinstalled-server-riscv64+unmatched.img.xz
+wget https://cdimage.ubuntu.com/releases/24.04/release/ubuntu-24.04.2-preinstalled-server-riscv64.img.xz
+xz -dk ubuntu-24.04.2-preinstalled-server-riscv64.img.xz
 ll /usr/lib/riscv64-linux-gnu/opensbi/generic/fw_jump.elf
 ll /usr/lib/u-boot/qemu-riscv64_smode/uboot.elf
-qemu-system-riscv64 -machine virt -nographic -m 2048 -smp 4 -bios /usr/lib/riscv64-linux-gnu/opensbi/generic/fw_jump.elf -kernel /usr/lib/u-boot/qemu-riscv64_smode/uboot.elf -device virtio-net-device,netdev=eth0 -netdev user,id=eth0 -drive file=ubuntu-20.04.5-preinstalled-server-riscv64+unmatched.img,format=raw,if=virtio
+qemu-system-riscv64 -machine virt -nographic -m 2048 -smp 4 -bios /usr/lib/riscv64-linux-gnu/opensbi/generic/fw_jump.elf -kernel /usr/lib/u-boot/qemu-riscv64_smode/uboot.elf -device virtio-net-device,netdev=eth0 -netdev user,id=eth0 -drive file=ubuntu-24.04.2-preinstalled-server-riscv64.img,format=raw,if=virtio
