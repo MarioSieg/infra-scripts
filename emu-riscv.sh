fda02ddb3215bc9b16b1b942f8175f524e2c07cd
Neo <mario.sieg.64@gmail.com>
2025-07-15 17:32:42 +0200

    Update emu-riscv.sh

--- emu-riscv.sh
+++ emu-riscv.sh
@@ -5,4 +5,4 @@ wget https://cdimage.ubuntu.com/releases/24.04/release/ubuntu-24.04.2-preinstall
 xz -dk ubuntu-24.04.2-preinstalled-server-riscv64.img.xz
 ll /usr/lib/riscv64-linux-gnu/opensbi/generic/fw_jump.elf
 ll /usr/lib/u-boot/qemu-riscv64_smode/uboot.elf
-qemu-system-riscv64 -machine virt -nographic -m 2048 -smp 4 -bios /usr/lib/riscv64-linux-gnu/opensbi/generic/fw_jump.elf -kernel /usr/lib/u-boot/qemu-riscv64_smode/uboot.elf -device virtio-net-device,netdev=eth0 -netdev user,id=eth0 -drive file=ubuntu-24.04.2-preinstalled-server-riscv64.img,format=raw,if=virtio
+qemu-system-riscv64 -machine virt -nographic -m 8192 -smp 8 -bios /usr/lib/riscv64-linux-gnu/opensbi/generic/fw_jump.elf -kernel /usr/lib/u-boot/qemu-riscv64_smode/uboot.elf -device virtio-net-device,netdev=eth0 -netdev user,id=eth0 -drive file=ubuntu-24.04.2-preinstalled-server-riscv64.img,format=raw,if=virtio
