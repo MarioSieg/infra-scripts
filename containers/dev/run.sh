4dae33ac376e5e5e0fbb93dace589468438a05eb
Neo <mario.sieg.64@gmail.com>
2025-07-22 15:09:40 +0200

    uiuiui

--- containers/dev/run.sh
+++ containers/dev/run.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-docker build -t neo-dev-image .
+read -rp "Git user name: " GIT_USER_NAME
+read -rp "Git user email: " GIT_USER_EMAIL
+read -rsp "GitHub Personal Access Token: " GIT_TOKEN
+echo
+
+docker build \
+  --build-arg GIT_USER_NAME="$GIT_USER_NAME" \
+  --build-arg GIT_USER_EMAIL="$GIT_USER_EMAIL" \
+  --build-arg GIT_TOKEN="$GIT_TOKEN" \
+  -t neo-dev-image .
 
 docker run -d \
   --name dev-container \
